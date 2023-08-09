cc16037139db7dc3ddefc368e1279d5d2c7aefbb
Aleksey Knyazev <ctj_yebbs@inbox.ru>
2023-08-09 09:41:40 +0300

    releasenotes version search improoved

--- oc_dltoolv2/releasenotes.py
+++ oc_dltoolv2/releasenotes.py
@@ -11,24 +11,23 @@ def get_possible_releasenotes_gavs(gav):
     :param gav:
     :return: array of found release notes for specified GAV
     """
-    version = parse_gav(gav)["v"]
-    if re.search("-\d+$", version):
-        build_independent_version = version.rsplit("-", 1)[0]
-        build_independent_gav = gav.replace(version, build_independent_version)
-        get_searched_gavs = lambda initial_gav: [initial_gav, initial_gav.replace(version, build_independent_version)]
-    else:
-        get_searched_gavs = lambda initial_gav: [initial_gav, ]
-    possible_releasenotes_gavs = []
-    citype_code = CheckSumsController().ci_type_by_path(gav, "NXS")
-    if citype_code:
-        group_releasenotes_gav = CheckSumsController().get_rn_gav(citype_code, version)
-        if group_releasenotes_gav:
-            possible_releasenotes_gavs.extend(get_searched_gavs(group_releasenotes_gav))
+    _version = parse_gav(gav).get("v")
+    _version_regexp = re.compile("[\.\-]")
+    _versions = list()
+
+    while _version:
+        _versions.append(_version)
+        _version = _version[:-len(_version_regexp.split(_version).pop())].rstrip(".").rstrip("-")
+
+    _csc = CheckSumsController()
+    citype_code = _csc.ci_type_by_path(gav, "NXS")
+    possible_releasenotes_gavs = list(map(lambda x: _csc.get_rn_gav(citype_code, x), _versions))
+
     # many components have artifactid like 'CODE-postfix'
     # where CODE is artifactid of releasenotes
-    artifactid = parse_gav(gav)["a"]
-    component_code = artifactid.rsplit("-", 1)[0]
+    artifactid = parse_gav(gav).get("a")
+    component_code = artifactid.rsplit("-", 1).pop(0)
     test_group = CiTypeGroups(code="tmp", name="tmp", rn_artifactid=component_code)
-    component_releasenotes_gav = test_group.get_rn_gav(version)
-    possible_releasenotes_gavs.extend(get_searched_gavs(component_releasenotes_gav))
-    return possible_releasenotes_gavs
+    component_releasenotes_gav = list(map(lambda x: test_group.get_rn_gav(x), _versions))
+    possible_releasenotes_gavs.extend(component_releasenotes_gav)
+    return list(filter(lambda x: bool(x), possible_releasenotes_gavs))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 included_packages = ["oc_dltoolv2"]
 
-__version = '3.11.2'
+__version = '3.11.3'
 
 spec = { "name": "oc-dltool",
          "version": __version,
